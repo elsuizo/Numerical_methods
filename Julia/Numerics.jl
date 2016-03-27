796f62d2b4362f88065f36c949d299d9bc277eb6
elsuizo <martin.noblia@openmailbox.org>
2016-03-27 20:16:44 -0300

    lu fact working

--- Julia/Numerics.jl
+++ Julia/Numerics.jl
@@ -6,7 +6,7 @@ module Numeric
 
 
 """
-`forward_subs(l, b)`
+`forward_subs(L, b)`
 
 compute the forward substitution algorithm to solve linear system
 
@@ -74,4 +74,50 @@ function backward_subs{T<:Number}(U::Array{T,2}, b::Array{T,1})
     return x
 end
 
+"""
+`lu_fact(A)`
+
+Compute the LU factorization of a matrix A
+
+input:
+------
+A(Array{Number, 2}) Matrix 
+
+Outputs:
+-------
+L(Array{Number, 2}) Lower triangle matrix
+U(Array{Number, 2}) Upper triangle matrix
+"""
+function lu_fact{T<:Number}(A::Array{T, 2})
+    
+    n, m = size(A)
+    L = zeros(A) 
+    U = zeros(A) 
+    if (n != m) 
+        error("The matrix must be square")
+    end
+    
+    if minimum(abs(diag(A))) == 0 
+        error("The system is singular")
+    end
+    for k in 1:n-1
+        for i in k+1:n
+            if (A[i, k] != zero(T))
+                γ = A[i, k] / A[k, k]
+                A[i, k+1:n] = A[i, k+1:n] - γ * A[k, k+1:n]
+                A[i, k] = γ 
+            end
+        end
+    end
+    L = tril(A, -1) + eye(n)
+    U = triu(A)
+    return L, U
+
+end
+
+
+
+#-------------------------------------------------------------------------
+# end of Numerics
+#-------------------------------------------------------------------------
 end
